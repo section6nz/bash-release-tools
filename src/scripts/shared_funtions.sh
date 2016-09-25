bf1d64aa301e2d62aef6f687ea8cb7d52692e527
Clemens Berndt <clemens.berndt@section6.nz>
2016-09-25 19:08:27 +1300

    Updated license information

--- src/scripts/shared_funtions.sh
+++ src/scripts/shared_funtions.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# Copyright (c) 2016 SECTION6 Limited. All rights reserved.
+#
+# This file is part of Bash Release Tools.
+#
+# Bash Release Tools is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Bash Release Tools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with Bash Release Tools. If not, see <http://www.gnu.org/licenses/>.
+
 # Outputs a warning message with the specified content.
 function warning {
   echo -e "[WARNING] $1" >&2
